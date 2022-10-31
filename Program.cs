2aca337d61036f2308c5750fff8f959761f14a3f
Carlos Decker <carlosdecker022@gmail.com>
2022-10-30 22:16:21 -0300

    Exercício completo

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.ForegroundColor = ConsoleColor.White;
+System.Console.WriteLine("--Multiplo--\n");
+System.Console.Write("Digite um número:        ");
+int numero = Convert.ToInt32(Console.ReadLine()!);
+System.Console.Write("Avalie se é múltiplo de: ");
+int multiplo = Convert.ToInt32(Console.ReadLine()!);
+if (numero % multiplo == 0)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    System.Console.WriteLine($"{numero} é multiplo de {multiplo}\n");
+    Console.ResetColor();
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    System.Console.WriteLine($"{numero} não é múltiplo de {multiplo}\n");
+    Console.ResetColor();
+}
+
+System.Console.Write("Pressione uma tecla para fechar...");
+Console.ReadKey();
